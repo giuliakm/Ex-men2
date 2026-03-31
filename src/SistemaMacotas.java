9cf4641d538257d079b9eecdd932ce4638e09a54
Giuliana KM <giuliakm@example.com>
2026-03-30 21:30:26 -0500

    Crear  el método de Registrar Mascota

--- src/SistemaMacotas.java
+++ src/SistemaMacotas.java
@@ -1,3 +1,37 @@
+import java.util.ArrayList;
 public class SistemaMacotas {
-    
+    private ArrayList<Mascota> lista;
+    public SistemaMacotas () {
+        lista = new ArrayList<>();
+    }
+
+    public void RegistrarMascota (int tipo, int id, String nombre, int edad){
+        if (id <= 0){
+            System.out.println("ID inválido");
+            return;
+        }
+
+        if (nombre.isEmpty()){
+            System.out.println("Nombre vacío");
+            return;
+        }
+
+        if (edad <= 0){
+            System.out.println("Edad inválida");
+            return;
+        }
+
+        Mascota m;
+        if (tipo == 1) {
+            m = new Perro(id, nombre, edad);
+        } else if (tipo == 2) {
+            m = new Gato(id, nombre, edad);
+        } else {
+            System.out.println("Tipo inválido");
+            return;
+        }
+        lista.add(m);
+        System.out.println("Mascota registrada correctamente");
+    }
+
 }
