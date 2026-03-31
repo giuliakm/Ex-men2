21f1c9b91125101d6ae1d071d7de280d979f50ac
Giuliana KM <giuliakm@example.com>
2026-03-30 20:47:39 -0500

    Crear clase gato

--- src/Gato.java
+++ src/Gato.java
@@ -1,3 +1,10 @@
-public class Gato {
-    
+public class Gato extends Mascota {
+    public Gato (int id, String nombre, int edad){
+        super(id, nombre, edad);
+    }
+
+    @Override
+    public String Sonido () {
+        return "Miau Miau";
+    }
 }
