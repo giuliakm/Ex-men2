7c2103a0a08eae6289c0c113717fcf5c91006b5d
Giuliana KM <giuliakm@example.com>
2026-03-30 21:35:47 -0500

    Crear el menu principal.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner tec =  new Scanner(System.in);
+        SistemaMacotas sistema =  new SistemaMacotas();
+
+        int opcion = 0;
+
+        do { 
+            System.out.println("\n--- SISTEMA DE MASCOTAS ---");
+            System.out.println("1. Registrar mascota");
+            System.out.println("2. Mostrar mascotas");
+            System.out.println("3. Ejecutar sonido");
+            System.out.println("4. Buscar por nombre");
+            System.out.println("5. Salir");
+        } while (opcion != 5);
     }
 }
